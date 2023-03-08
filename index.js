8896b40fefffcc8016af88636dbcbd168629ebfa
Musabbir-pr <musabbir313@gmail.com>
2023-03-08 20:20:54 +0600

    added verify and update method for sellers

--- index.js
+++ index.js
@@ -1,80 +1,172 @@
-const express= require('express')
-const cors= require('cors')
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
-const app= express()
-const port= process.env.PORT || 5000
-app.use(cors())
-app.use(express.json())
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //mongodb
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@cluster0.z1jayhr.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
+  try {
+    // Collectins
+    const categoryCollection = client.db("boatFinder").collection("categories");
+    const userCollection = client.db("boatFinder").collection("users");
+    const boatCollection = client.db("boatFinder").collection("boats");
 
-    try{
-        // Collectins
-        const categoryCollection= client.db('boatFinder').collection('categories')
-        const userCollection= client.db('boatFinder').collection('users')
-            
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const cursor = categoryCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-        app.get( '/categories', async (req,res)=>{
-            const query= {}
-            const cursor= categoryCollection.find(query)
-            const result= await cursor.toArray()
-            res.send(result)
-        })
+    //get all products under a category
 
-        //save user
-        app.post('/user', async(req,res)=> {
-            const user= req.body
-            const result= await userCollection.insertOne(user)
-            res.send(result)
-            console.log(result);
-        })
+    app.get("/category/:id", async (req, res) => {
+      const category = req.params.id;
+      const query = { productCategory: category };
+      const products = await boatCollection.find(query).toArray();
+      res.send(products);
+    });
 
-        //check admin role
+    //get sellers
+    app.get("/seller", async (req, res) => {
+      const query = { role: "seller" };
+      const result = await userCollection.find(query).toArray();
+      res.send(result);
+    });
 
-        app.get('/user/admin/:email', async(req, res)=> {
-            const email= req.params.email
-            const query= {email}
-            const user= await userCollection.findOne(query)
-            res.send({isAdmin: user?.role=== "admin"})
-        })
+    //get buyers
+    app.get("/buyer", async (req, res) => {
+      const query = { role: "buyer" };
+      const result = await userCollection.find(query).toArray();
+      res.send(result);
+    });
 
-        //check buyer role
-        app.get('/user/buyer/:email', async(req,res)=> {
-            const email= req.params.email
-            const query= {email}
-            const user= await userCollection.findOne(query)
-            res.send( {isBuyer: user?.role=== "buyer"})
-        })
+    // Get products added by a seller
+    app.get("/myproducts", async (req, res) => {
+      const email = req.query.email;
+      const query = { sellerEmail: email };
+      const products = await boatCollection.find(query).toArray();
+      res.send(products);
+    });
 
-     //check seller role
-     app.get('user/seller/:email', async(req,res)=>{
-        const email= req.params.email 
-        const query= {email}
-        const user= await userCollection.findOne(query)
-        res.send({isSeller: user?.role==="seller"})
-     })
-    }
+    //advertise product
+    app.put("/advertise/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const advertiseStatus = req.body;
+      const option = { upsert: true };
+      const updataAdvertise = {
+        $set: {
+          advertised: advertiseStatus.advertised,
+        },
+      };
+      const result = await boatCollection.updateOne(
+        filter,
+        updataAdvertise,
+        option
+      );
 
-   finally{
+      res.send(result);
+    });
 
-   }
+    //delete product
+    app.delete("/myproductdelete/:id", async (req, res) => {
+      const id = req.params.id;
+      const remove = { _id: new ObjectId(id) };
+      const result = await boatCollection.deleteOne(remove);
+      res.send(result);
+    });
+
+    //add product
+    app.post("/product", async (req, res) => {
+      const product = req.body;
+      const result = await boatCollection.insertOne(product);
+      res.send(result);
+    });
+
+    //save user
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+      console.log(result);
+    });
+
+    //delete a seller
+    app.delete("/seller/:id", async (req, res) => {
+      const id = req.params.id;
+      const remove = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(remove);
+      res.send(result);
+    });
+
+    // verify seller
+    app.put("/seller/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const sellerStatus = req.body;
+      const option = { upsert: true };
+      const updateSeller = {
+        $set: {
+          verified: sellerStatus.verified,
+        },
+      };
+      const result = await userCollection.updateOne(
+        filter,
+        updateSeller,
+        option
+      );
+
+      res.send(result);
+    });
+
+    //check admin role
+
+    app.get("/user/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await userCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
+    });
+
+    //check buyer role
+    app.get("/user/buyer/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await userCollection.findOne(query);
+      res.send({ isBuyer: user?.role === "buyer" });
+    });
+
+    //check seller role
+    app.get("/user/seller/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = await userCollection.findOne(query);
+      res.send({ isSeller: user?.role === "seller" });
+      console.log(user);
+    });
+  } finally {
+  }
 }
 
-run().catch(err=>console.log(err))
+run().catch((err) => console.log(err));
 
+app.get("/", (req, res) => {
+  res.send("Boatfinder server is running");
+});
 
-app.get('/', (req,res)=>{
-    res.send("Boatfinder server is running")
-})
-
-app.listen(port, ()=>{
-    console.log(`server running on port ${port} `);
-})
-
+app.listen(port, () => {
+  console.log(`server running on port ${port} `);
+});
